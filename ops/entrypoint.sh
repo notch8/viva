a6f3c387bab173f5ecb65c2c18c2fff826a018f2
April Rieger <april.rieger@yahoo.com>
2023-06-15 18:59:13 -0700

    tweaks

--- ops/entrypoint.sh
+++ ops/entrypoint.sh
@@ -1,11 +1,5 @@
-#!/bin/bash -l
+#!/bin/bash
 set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
-
-bundle check || bundle install
-
-echo "Starting $@"
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+rm -f tmp/pids/server.pid
+exec bundle exec rails s -p 3000 -b '0.0.0.0'

d7ecfa00f3f4e6fd81652b042e0fca471c76201c
April Rieger <april.rieger@yahoo.com>
2023-06-16 19:22:21 -0700

    adding starter page routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/starter', to: 'starter#index', as: 'starter'
   # Defines the root path route ("/")
   # root "articles#index"
 end

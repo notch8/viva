4f198414588d372ec0d6c6956291089b00b495d8
April Rieger <april.rieger@yahoo.com>
2023-06-16 20:51:56 -0700

    sanity check, will this work on a created page with route/controller/view?

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get '/starter', to: 'starter#index', as: 'starter'
   # Defines the root path route ("/")
-  # root "articles#index"
+  # get '/starter', to: 'starter#index', as: 'starter'
+  root "pages#index"
 end

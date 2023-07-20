421056730b21f4d0c150be29de1a2a55648cea3c
Summer Cook <summer@scientist.com>
2023-07-20 13:13:57 -0300

    root to devise login for unauthenticated & react app once authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # get '/starter', to: 'starter#index', as: 'starter'
-  root "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
 end

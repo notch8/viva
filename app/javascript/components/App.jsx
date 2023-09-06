727d327cdcca1ad0ae3b0d30bca06c954f991a77
Alisha Evans <alishaevn2@gmail.com>
2023-09-06 17:13:49 -0500

    add code comment

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Router } from 'wouter'
 import Settings from './Settings'
 import Header from './ui/Header'
 
+// NOTE: Whatever you put here does not go away when being navigated to a different page. Use for routes only.
 const App = () => {
   return (
     <Router>

833f253787641c53aeeb46be231715cac7a07f76
Alisha Evans <alishaevn2@gmail.com>
2023-09-07 09:23:49 -0500

    rubocop fix

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+##
+# The controller to handle methods related to the settings page.
 class SettingsController < ApplicationController
   def index
     render inertia: 'Settings'

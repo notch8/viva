382e5acc40ef06f19c733a530897b7dbe65058c4
leaannbradford <ltrammer@gmail.com>
2023-09-07 10:46:42 -0700

    WIP work on filtering by keyword

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,12 @@ class Question < ApplicationRecord
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :keywords
   validates :text, presence: true
+
+  # Filter questions by filter and category
+  def self.filter(keywords: [], categories: [])
+    questions = Question.all
+    # TODO: filter only when all keywords are a match
+    questions = questions.where(id: Keyword.joins(:questions).where(name: keywords).pluck(:question_id)) if keywords.present?
+    questions
+  end
 end

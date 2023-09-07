5351b9b93e7c538451e0050ce90d31505b9e0d1d
leaannbradford <ltrammer@gmail.com>
2023-09-07 08:48:27 -0700

    add validations, relationships and tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :keywords
+  validates :text, presence: true
 end

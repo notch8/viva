dbfb5774eee818cab14273ba718f29d6555faac4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-09-07 17:11:26 -0400

    🎁 Expose the keywords and categories to the App component

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,9 @@ require 'faker'
 class HomeController < ApplicationController
   def index
     render inertia: 'App', props: {
-      name: Faker::Name.name
+             name: Faker::Name.name,
+             keywords: Keyword.all.pluck(:name),
+             categories: Category.all.pluck(:name),
     }
   end
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HomeController do
+  describe '#index', inertia: true do
+    it  do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :index
+
+      expect_inertia.to render_component 'App'
+      expect(inertia.props[:keywords]).to be_a(Array)
+      expect(inertia.props[:categories]).to be_a(Array)
+    end
+  end
+end

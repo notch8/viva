bb04d55d51943f15feaa21da6fdfb557249c803a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-09-12 12:17:11 -0400

    ♻️ Adjust spec for clarity
    Co-authored-by: Summer Cook <summer@scientist.com>

--- spec/models/question/select_all_that_apply_spec.rb
+++ spec/models/question/select_all_that_apply_spec.rb
@@ -8,16 +8,43 @@ RSpec.describe Question::SelectAllThatApply do
   describe 'data serialization' do
     subject { FactoryBot.build(:question_select_all_that_apply, data:) }
     [
-      ["Green::t|Blue::false|Red::1|Yellow::0", [["Green", true], ["Blue", false], ["Red", true], ["Yellow", false]], true],
-      ["Green|Blue::false", [["Green"], ["Blue", false]], false],
-      ["A::t|B::t|C::f", [["A", true], ["B", true], ["C", false]], true],
-      [nil, nil, false],
-      # We only
-      ["Green::t::Yellow|t", [["Green", true, "Yellow"], ["t"]], false],
-      ["", [], false],
-      [[["A", true], ["B", false]], [["A", true], ["B", false]], true],
-      # Highlighting that for now all answers could be incorrect.
-      ["Green::false|Blue::false", [["Green", false], ["Blue", false]], true]
+      [
+        "Green::t|Blue::false|Red::1|Yellow::0",
+        [["Green", true], ["Blue", false], ["Red", true], ["Yellow", false]],
+        true
+      ], [
+        "Green|Blue::false",
+        [["Green"], ["Blue", false]],
+        false
+      ], [
+        "A::t|B::t|C::f",
+        [["A", true], ["B", true], ["C", false]],
+        true
+      ], [
+        nil,
+        nil,
+        false
+      ], [
+        # We only want A::t|B::f format; this is a triple (instead of pair) that is parsed but invalid
+        "Green::t::Yellow|t",
+        [["Green", true, "Yellow"], ["t"]],
+        false
+      ], [
+        "",
+        [],
+        false
+      ],
+      [
+        [["A", true], ["B", false]],
+        [["A", true], ["B", false]],
+        true
+      ], [
+        # TODO: We probably want to enforce that all questions have at least one correct answer.
+        # Highlighting that for now all answers could be incorrect.
+        "Green::false|Blue::false",
+        [["Green", false], ["Blue", false]],
+        true
+      ]
     ].each do |given, expected, valid|
       context "when given #{given.inspect}" do
         let(:data) { given }

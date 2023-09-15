db0cc82ca0d0428d8f15266b3aeda9c191a8b145
Alisha Evans <alishaevn2@gmail.com>
2023-09-15 14:36:16 -0500

    update sample data for traditional questions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,43 +8,83 @@ class SearchController < ApplicationController
       keywords: Keyword.all.pluck(:name),
       categories: Category.all.pluck(:name),
       types: Question.types,
-    #   filtered_questions: [
-    #     {
-    #       id: 1,
-    #       text: 'What is the meaning of life?',
-    #       answers: ['This is an answer to the meaning of life.', 'This is another answer to the meaning of life.', 'This is a third answer to the meaning of life.', 'This is a fourth answer to the meaning of life.'],
-    #       incorrect_answers: ['one'],
-    #       type: 'Question::Traditional',
-    #       keywords: ['meaning', 'life'],
-    #       categories: ['history'],
-    #       level: '1',
-    #     },
-    #     {
-    #       id: 2,
-    #       text: 'What is the meaning of life?',
-    #       answers: ['This is an answer to the meaning of life.', 'This is another answer to the meaning of life.', 'This is a third answer to the meaning of life.', 'This is a fourth answer to the meaning of life.'],
-    #       incorrect_answers: [],
-    #       type: 'Question::Traditional',
-    #       keywords: ['meaning', 'life'],
-    #       categories: ['history'],
-    #       level: '1',
-    #     },
-    #     {
-    #       id: 3,
-    #       text: 'What is the meaning of life?',
-    #       answers: ['This is an answer to the meaning of life.', 'This is another answer to the meaning of life.', 'This is a third answer to the meaning of life.', 'This is a fourth answer to the meaning of life.'],
-    #       type: 'Question::Traditional',
-    #       keywords: ['meaning', 'life'],
-    #       categories: ['nursing'],
-    #       level: '1',
-    #     },
-    #   ]
-    # }
       filtered_questions: Question.filter_as_json(
-                keywords: params[:keywords],
-                categories: params[:categories],
-                type: params[:type]
-              )
-            }
+        keywords: params[:keywords],
+        categories: params[:categories],
+        type: params[:type]
+      )
+      # filtered_questions: [
+      #   {
+      #     id: 1,
+      #     text: 'What is the meaning of life?',
+      #     data: [
+      #       {
+      #         text: 'This is an answer to the meaning of life.',
+      #         correct: true
+      #       }, {
+      #         text: 'This is another answer to the meaning of life.',
+      #         correct: false
+      #       }, {
+      #         text: 'This is a third answer to the meaning of life.',
+      #         correct: false
+      #       }, {
+      #         text: 'This is a fourth answer to the meaning of life.',
+      #         correct: false
+      #       }
+      #     ],
+      #     type: 'Question::Traditional',
+      #     keywords: ['meaning', 'life'],
+      #     categories: ['history'],
+      #     level: '1',
+      #   },
+      #   {
+      #     id: 2,
+      #     text: 'What is the meaning of life?',
+      #     data: [
+      #       {
+      #         text: 'This is an answer to the meaning of life.',
+      #         correct: true
+      #       }, {
+      #         text: 'This is another answer to the meaning of life.',
+      #         correct: false
+      #       }, {
+      #         text: 'This is a third answer to the meaning of life.',
+      #         correct: false
+      #       }, {
+      #         text: 'This is a fourth answer to the meaning of life.',
+      #         correct: false
+      #       }
+      #     ],
+      #     type: 'Question::Traditional',
+      #     keywords: ['meaning', 'life'],
+      #     categories: ['history'],
+      #     level: '1',
+      #   },
+      #   {
+      #     id: 3,
+      #     text: 'What is the meaning of life?',
+      #     data: [
+      #       {
+      #         text: 'This is an answer to the meaning of life.',
+      #         correct: true
+      #       }, {
+      #         text: 'This is another answer to the meaning of life.',
+      #         correct: false
+      #       }, {
+      #         text: 'This is a third answer to the meaning of life.',
+      #         correct: false
+      #       }, {
+      #         text: 'This is a fourth answer to the meaning of life.',
+      #         correct: false
+      #       }
+      #     ],
+      #     type: 'Question::Traditional',
+      #     keywords: ['meaning', 'life'],
+      #     categories: ['nursing'],
+      #     level: '1',
+      #   },
+      # ]
+      }
+    }
   end
 end
--- app/javascript/components/ui/Answers/index.jsx
+++ app/javascript/components/ui/Answers/index.jsx
@@ -8,8 +8,9 @@ const Answers = ({ question_type, answers }) => {
     <>
       <h3 className='h6 fw-bold default-answers'>Answers</h3>
       {question_type === 'Question::BowTie' && <BowTieAnswers answers={answers} />}
-      {question_type === 'Question::Matching' && <MatchingAnswers answers={answers} />}
       {question_type === 'Question::DragAndDrop' && <DragAndDropAnswers answers={answers} />}
+      {question_type === 'Question::Matching' && <MatchingAnswers answers={answers} />}
+      {question_type === 'Question::StimulusCaseStudy' && <StimulusCaseStudyAnswers answers={answers} />}
 
       {/* All other question types use the same format */}
       {answers.map((answer, index) => {

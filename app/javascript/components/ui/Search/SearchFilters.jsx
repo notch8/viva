65c1dc6e68f9c47ca24dba99fb63cfd9e6824949
Summer Cook <summer@scientist.com>
2023-09-18 17:42:54 -0600

    styling and responsiveness

--- app/javascript/components/ui/Search/SearchFilters.jsx
+++ app/javascript/components/ui/Search/SearchFilters.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Col, Button, CloseButton } from 'react-bootstrap'
+import { Container, Row, Col, Button, CloseButton } from 'react-bootstrap'
 
 const SearchFilters = (props) => {
   // TODO: set up levels
@@ -19,34 +19,40 @@ const SearchFilters = (props) => {
 
   return (
     hasFilters &&
-    <Container className='bg-light-1 rounded d-flex p-0'>
-    <Col sm={4} className='d-flex justify-content-center align-items-center text-center p-2 border-end'>
-      <h2 className='h5 fw-bold'>Selected Filters</h2>
-    </Col>
-    <Col sm={8}>
-      {filterArray.map((filter, index) => (
-            arrayHasItems(filter) && (
-              <div key={index} className='p-2'>
-                <h3 className="fw-bold h6">
-                  {filter === selectedCategories ? 'Categories' : filter === selectedKeywords ? 'Keywords' : 'Types'}
-                </h3>
-                {filter.map((item, itemIndex) => (
-                  <div key={itemIndex} className='m-1 btn bg-white text-lowercase d-inline-flex align-items-center' variant='secondary'>
-                    <label>{item}</label>
-                    <CloseButton
-                      aria-label={`Remove filter for ${item}`}
-                      onClick={(event) => removeFilterAndSearch(event, item, filter)}
-                    />
-                  </div>
+      <Container className='bg-light-1 rounded p-0'>
+        <Row>
+          <Col sm={3} className='d-flex justify-content-center align-items-center text-center p-2 border-end'>
+            <h2 className='h5 fw-bold'>Selected Filters</h2>
+          </Col>
+          <Col sm={9}>
+            <Container>
+              <Row className='py-2'>
+                {filterArray.map((filter, index) => (
+                  arrayHasItems(filter) && (
+                    <Col key={index} sm={6}>
+                      <h3 className="fw-bold h6">
+                        {filter === selectedCategories ? 'Categories' : filter === selectedKeywords ? 'Keywords' : 'Types'}
+                      </h3>
+                      {filter.map((item, itemIndex) => (
+                        <div key={itemIndex} className='m-1 btn bg-white text-lowercase d-inline-flex align-items-center' variant='secondary'>
+                          <label>{item}</label>
+                          <CloseButton
+                            aria-label={`Remove filter for ${item}`}
+                            onClick={(event) => removeFilterAndSearch(event, item, filter)}
+                          />
+                        </div>
+                      ))}
+                    </Col>
+                  )
                 ))}
-              </div>
-            )
-          ))}
-      <Col className='d-flex justify-content-end align-items-end border-top bg-light-2 p-2'>
-        <Button>Export All Questions</Button>
-      </Col>
-    </Col>
-  </Container>
+              </Row>
+            </Container>
+          <Col className='d-flex justify-content-end align-items-end border-top bg-light-2 p-2'>
+            <Button>Export All Questions</Button>
+          </Col>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 

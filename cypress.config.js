58bc5865d6f4a1fe376a6f8851b2aeeecd882933
Alisha Evans <alishaevn2@gmail.com>
2023-09-18 16:05:38 -0500

    fix the autocorrectable errors

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   component: {
@@ -20,4 +20,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+})

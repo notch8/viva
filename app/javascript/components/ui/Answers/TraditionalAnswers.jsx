19910cc3a4f500cdb0c9ff93787f0cbd1f1ebdaf
Summer Cook <summer@scientist.com>
2023-09-25 16:49:40 -0600

    fix 2 small style things

--- app/javascript/components/ui/Answers/TraditionalAnswers.jsx
+++ app/javascript/components/ui/Answers/TraditionalAnswers.jsx
@@ -8,13 +8,13 @@ const TraditionalAnswers = ({ answers }) => {
     answers && answers.map((answer, index) => {
       return (
         <Row className={`rounded m-1 p-1 d-flex align-items-center justify-content-center ${answer.correct ? 'correct' : ''}`} key={index}>
-          <Col sm={2}>
+          <Col xs={2} lg={1} className='px-0'>
             <Button variant='primary' className='m-1'>{index}</Button>
           </Col>
-          <Col sm={10}>
+          <Col xs={10} sm={7} lg={8}>
             <span>{answer.answer}</span>
           </Col>
-          <Col sm={2} className='align-self-end ms-auto'>
+          <Col sm={3} className='d-flex justify-content-center justify-content-md-end align-self-end ms-auto px-0'>
             {answer.correct &&
               <Badge bg='light' text='dark' className='ms-auto'>CORRECT</Badge>
             }
--- app/javascript/components/ui/Search/SearchFilters.jsx
+++ app/javascript/components/ui/Search/SearchFilters.jsx
@@ -23,10 +23,10 @@ const SearchFilters = (props) => {
     hasFilters &&
       <Container className='bg-light-1 rounded p-0 search-filters'>
         <Row>
-          <Col sm={3} className='d-flex justify-content-center align-items-center text-center p-2 border-end'>
+          <Col md={3} className='d-flex justify-content-center align-items-center text-center p-2 border-end'>
             <h2 className='h5 fw-bold'>Selected Filters</h2>
           </Col>
-          <Col sm={9}>
+          <Col md={9} className='ps-md-0'>
             <Container>
               <Row className='py-2'>
                 {filterArray.map((filter, index) => (
@@ -50,7 +50,7 @@ const SearchFilters = (props) => {
                 ))}
               </Row>
             </Container>
-            <Col className='d-flex justify-content-end align-items-end border-top bg-light-2 p-2'>
+            <Col className='d-flex justify-content-center justify-content-md-end align-items-end border-top bg-light-2 p-2'>
               <Button>Export All Questions</Button>
             </Col>
           </Col>

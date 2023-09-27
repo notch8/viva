60f80d2a8ee299834ac72a281d7dc80d289eb41e
Summer Cook <summer@scientist.com>
2023-09-27 15:17:51 -0600

    a few more changes in the UI to make levels work

--- app/javascript/components/ui/Search/SearchFilters.jsx
+++ app/javascript/components/ui/Search/SearchFilters.jsx
@@ -4,7 +4,6 @@ import {
 } from 'react-bootstrap'
 
 const SearchFilters = (props) => {
-  // TODO: set up levels
   const { selectedSubjects, selectedKeywords, selectedTypes, selectedLevels, submit, handleFilters } = props
   const filterArray = [selectedSubjects, selectedKeywords, selectedTypes, selectedLevels]
 

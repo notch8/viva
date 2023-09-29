34719cae30d12cefe7fe8097b7376dc00d080975
Summer Cook <summer@scientist.com>
2023-09-29 14:40:48 -0600

    fix borders

--- app/javascript/components/ui/Answers/MatchingAnswers.jsx
+++ app/javascript/components/ui/Answers/MatchingAnswers.jsx
@@ -5,14 +5,14 @@ const MatchingAnswers = ({ answers }) => {
   return (
     <Table bordered className='bg-white'>
       <tbody>
-        { answers.map((answer, index) => {
+        {answers.map((answer, index) => {
           console.log(answer)
           return (
-            <tr key={index}>
-              <td className='px-3 text-primary fw-semibold matcher align-middle text-center'>{answer.answer}</td>
-              <td className='p-0 d-flex flex-column'>
+            <tr className='matching-table-row' key={index}>
+              <td className='px-3 text-primary fw-semibold matchee align-middle text-center'>{answer.answer}</td>
+              <td className='p-0 d-flex flex-column matcher'>
                 {answer.correct && answer.correct.map((match, index) => (
-                  <span className='px-3 py-2 border-bottom' key={match + index}>{match}</span>
+                  <span className='px-3 py-2' key={match + index}>{match}</span>
                 ))}
               </td>
             </tr>

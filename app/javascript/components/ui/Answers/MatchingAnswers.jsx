4b107382651571bb910f32c69b8dd165bc1cd451
Summer Cook <summer@scientist.com>
2023-09-29 14:22:27 -0600

    UI for matching component

--- app/javascript/components/ui/Answers/MatchingAnswers.jsx
+++ app/javascript/components/ui/Answers/MatchingAnswers.jsx
@@ -6,10 +6,15 @@ const MatchingAnswers = ({ answers }) => {
     <Table bordered className='bg-white'>
       <tbody>
         { answers.map((answer, index) => {
+          console.log(answer)
           return (
             <tr key={index}>
-              <td className='px-3 text-primary fw-semibold'>{answer.answer}</td>
-              <td className='px-3'>{answer.correct}</td>
+              <td className='px-3 text-primary fw-semibold matcher align-middle text-center'>{answer.answer}</td>
+              <td className='p-0 d-flex flex-column'>
+                {answer.correct && answer.correct.map((match, index) => (
+                  <span className='px-3 py-2 border-bottom' key={match + index}>{match}</span>
+                ))}
+              </td>
             </tr>
           )
         })}

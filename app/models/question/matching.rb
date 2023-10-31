481fa537bde96207a7ab050060b180f6f4e67cfa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-10-31 12:45:17 -0400

    🎁 Improve Matching validation messages for import
    Prior to this commit, the validation error messages sent to the person
    uploading a Matching question talked about hashes and arrays;
    something that was not part of their CSV.
    
    With this commit, we're trying to provide guidance as to the CSV
    validation by referencing named columns.
    
    Related to:
    
    - https://github.com/scientist-softserv/viva/issues/183

--- app/models/question/matching.rb
+++ app/models/question/matching.rb
@@ -7,32 +7,60 @@
 class Question::Matching < Question
   self.type_name = "Matching"
 
-  # rubocop:disable Metrics/MethodLength
-  def self.build_row(row)
-    text = row['TEXT']
-    level = row['LEVEL']
-    subject_names = extract_subject_names_from(row)
-    keyword_names = extract_keyword_names_from(row)
+  ##
+  # Represents the mapping process of a CSV Row to the underlying {Question::Matching}.
+  #
+  # The primary purpose of this class is to convey meaningful error messages for invalid CSV
+  # structures.
+  #
+  # @see {#validate_well_formed_row}
+  class ImportCsvRow < Question::ImportCsvRow
+    ##
+    # @see #validate_well_formed_row
+    #
+    # rubocop:disable Metrics/MethodLength
+    def extract_answers_and_data_from(row)
+      # These are reused in #validate_well_formed_row
+      @lefts = []
+      @rights = []
+      # Ensure that we have all of the candidate indices (the left and right side)
+      row.headers.each do |header|
+        next if header.blank?
 
-    # Ensure that we have all of the candidate indices (the left and right side)
-    indices = row.headers.each_with_object([]) do |header, array|
-      next if header.blank?
-      next unless header.start_with?("LEFT_", "RIGHT_")
-      array << header.split(/_+/).last.to_i
-    end.uniq.sort
+        if header.start_with?("LEFT_")
+          @lefts << header.split(/_+/).last.to_i
+        elsif header.start_with?("RIGHT_")
+          @rights << header.split(/_+/).last.to_i
+        end
+      end
 
-    data = indices.each_with_object([]) do |index, array|
-      # It is okay that these will possibly be nil; because our downstream validation will catch
-      # them.
-      answer = row["LEFT_#{index}"]
-      correct = row["RIGHT_#{index}"]&.split(/\s*,\s*/)
-      next if answer.blank? && correct.blank?
-      array << { answer:, correct: }
+      indices = (@lefts + @rights).uniq.sort
+
+      @data = indices.each_with_object([]) do |index, array|
+        # It is okay that these will possibly be nil; because our downstream validation will catch
+        # them.
+        answer = row["LEFT_#{index}"]
+        correct = row["RIGHT_#{index}"]&.split(/\s*,\s*/)
+        next if answer.blank? && correct.blank?
+        array << { "answer" => answer, "correct" => correct }
+      end
     end
+    # rubocop:enable Metrics/MethodLength
 
-    new(text:, data:, subject_names:, keyword_names:, level:)
+    def validate_well_formed_row
+      return unless @lefts.sort != @rights.sort
+      message = "mismatch of LEFT and RIGHT columns."
+      left_has = @lefts - @rights
+      message += " Have LEFT_#{left_has.join(', LEFT_')} columns without corresponding RIGHT_#{left_has.join(', RIGHT_')} columns." if left_has.any?
+      right_has = @rights - @lefts
+      message += " Have RIGHT_#{right_has.join(', RIGHT_')} columns without corresponding LEFT_#{right_has.join(', LEFT_')} columns." if right_has.any?
+      errors.add(:data, message)
+    end
   end
-  # rubocop:enable Metrics/MethodLength
+
+  def self.build_row(row)
+    ImportCsvRow.new(question_type: self, row:)
+  end
 
   # NOTE: We're not storing this in a JSONB data type, but instead favoring a text field.  The need
   # for the data to be used in the application, beyond export of data, is minimal.

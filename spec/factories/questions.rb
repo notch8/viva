5d7f81c3e7ff492a2db8299a6a11a5d7e1a55db8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-12-05 11:10:30 -0500

    🧹 Re-arrange order of factories
    Instead of entered on a whim, re-arrange the factories in their
    alphabetical order.

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -34,74 +34,6 @@ FactoryBot.define do
       end
     end
 
-    # NOTE: These factory names are based on the class's model_name's param_key which helps with
-    # the ./spec/shared_examples.rb
-    factory :question_drag_and_drop, class: Question::DragAndDrop, parent: :question do
-      data { (1..6).map { |i| { answer: "Left #{i} #{Faker::Lorem.word}", correct: i.even? } } }
-    end
-
-    factory :question_traditional, class: Question::Traditional, parent: :question do
-      # In this case there are 4 candidate answers and the 3rd one is always correct (always C)
-      data { (1..4).map { |i| { answer: "Answer #{i} #{Faker::Lorem.word}", correct: i == 3 } } }
-    end
-
-    factory :question_matching, class: Question::Matching, parent: :question do
-      data do
-        (1..4).map do |i|
-          {
-            answer: "Left #{i} #{Faker::Lorem.word}",
-            correct: (0..rand(4)).map { |j| "Right #{i}-#{j} #{Faker::Lorem.word}" }
-          }
-        end
-      end
-    end
-
-    factory :question_categorization, class: Question::Categorization, parent: :question do
-      data do
-        (1..4).map do |i|
-          {
-            answer: "Left #{i} #{Faker::Lorem.word}",
-            correct: (0..rand(4)).map { |j| "Right #{i}-#{j} #{Faker::Lorem.word}" }
-          }
-        end
-      end
-    end
-
-    factory :question_essay, class: Question::Essay, parent: :question do
-      data { { "markdown" => "Hello\n* World\n\nLink to [Samvera](https://samvera.org)" } }
-    end
-
-    factory :question_upload, class: Question::Upload, parent: :question do
-      data { { "markdown" => "Hello\n* World\n\nLink to [Samvera](https://samvera.org)" } }
-    end
-
-    factory :question_scenario, class: Question::Scenario, parent: :question do
-      parent_question factory: :question_stimulus_case_study_without_children
-    end
-
-    factory :question_stimulus_case_study_without_children, class: Question::StimulusCaseStudy, parent: :question
-
-    factory :question_stimulus_case_study, class: Question::StimulusCaseStudy, parent: :question_stimulus_case_study_without_children do
-      after(:build) do |question, _context|
-        child_question_classes = Question.descendants.select(&:included_in_filterable_type?) - [question.class]
-
-        (0..5).map do |i|
-          # Injecting some scenarios into the questions.
-          child = if i == 0 || i == 3
-                    # If we don't specify the scenario's parent, we'll build an all new one.
-                    FactoryBot.build(:question_scenario, parent_question: question)
-                  else
-                    FactoryBot.build(child_question_classes.sample.model_name.param_key, child_of_aggregation: true)
-                  end
-          question.as_parent_question_aggregations.build(presentation_order: i, child_question: child)
-        end
-      end
-    end
-
-    factory :question_select_all_that_apply, class: Question::SelectAllThatApply, parent: :question do
-      data { [{ answer: "A", correct: true }, { answer: "B", correct: true }, { answer: "C", correct: false }] }
-    end
-
     factory :question_bow_tie, class: Question::BowTie, parent: :question do
       data do
         {
@@ -178,6 +110,74 @@ FactoryBot.define do
           }
         }
       end
+    end
+
+    factory :question_categorization, class: Question::Categorization, parent: :question do
+      data do
+        (1..4).map do |i|
+          {
+            answer: "Left #{i} #{Faker::Lorem.word}",
+            correct: (0..rand(4)).map { |j| "Right #{i}-#{j} #{Faker::Lorem.word}" }
+          }
+        end
+      end
+    end
+
+    # NOTE: These factory names are based on the class's model_name's param_key which helps with
+    # the ./spec/shared_examples.rb
+    factory :question_drag_and_drop, class: Question::DragAndDrop, parent: :question do
+      data { (1..6).map { |i| { answer: "Left #{i} #{Faker::Lorem.word}", correct: i.even? } } }
+    end
+
+    factory :question_essay, class: Question::Essay, parent: :question do
+      data { { "markdown" => "Hello\n* World\n\nLink to [Samvera](https://samvera.org)" } }
+    end
+
+    factory :question_matching, class: Question::Matching, parent: :question do
+      data do
+        (1..4).map do |i|
+          {
+            answer: "Left #{i} #{Faker::Lorem.word}",
+            correct: (0..rand(4)).map { |j| "Right #{i}-#{j} #{Faker::Lorem.word}" }
+          }
+        end
+      end
+    end
+
+    factory :question_scenario, class: Question::Scenario, parent: :question do
+      parent_question factory: :question_stimulus_case_study_without_children
+    end
+
+    factory :question_select_all_that_apply, class: Question::SelectAllThatApply, parent: :question do
+      data { [{ answer: "A", correct: true }, { answer: "B", correct: true }, { answer: "C", correct: false }] }
+    end
+
+    factory :question_stimulus_case_study_without_children, class: Question::StimulusCaseStudy, parent: :question
+
+    factory :question_stimulus_case_study, class: Question::StimulusCaseStudy, parent: :question_stimulus_case_study_without_children do
+      after(:build) do |question, _context|
+        child_question_classes = Question.descendants.select(&:included_in_filterable_type?) - [question.class]
+
+        (0..5).map do |i|
+          # Injecting some scenarios into the questions.
+          child = if i == 0 || i == 3
+                    # If we don't specify the scenario's parent, we'll build an all new one.
+                    FactoryBot.build(:question_scenario, parent_question: question)
+                  else
+                    FactoryBot.build(child_question_classes.sample.model_name.param_key, child_of_aggregation: true)
+                  end
+          question.as_parent_question_aggregations.build(presentation_order: i, child_question: child)
+        end
+      end
+    end
+
+    factory :question_traditional, class: Question::Traditional, parent: :question do
+      # In this case there are 4 candidate answers and the 3rd one is always correct (always C)
+      data { (1..4).map { |i| { answer: "Answer #{i} #{Faker::Lorem.word}", correct: i == 3 } } }
+    end
+
+    factory :question_upload, class: Question::Upload, parent: :question do
+      data { { "markdown" => "Hello\n* World\n\nLink to [Samvera](https://samvera.org)" } }
     end
   end
 end

ac05a44d552e796a6f8ae37a122168199d29ed56
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-12-05 13:53:35 -0500

    🎁 Provide HTML output of data for "Rich Text" questions
    Prior to this commit, we were exposing markdown as part of the data for
    rendering on the client side.
    
    After discussion with team members and thinking through the
    implications, we chose to switch to providing the React front-end with
    HTML.
    
    The thought being:
    
    1. We can sanitize the HTML according to consistent server rules; which
       will be similar logic we'd provide during the export of data to QTI
       format.  And since the export format will come from the server, it
       makes sense for the server to handle the markdown to HTML conversion
       and sanitization.
    2. By providing HTML, we're telling the client-side application that
       we're promising HTML.  Something that is a more reasonable promise
       than Markdown.
       - While markdown might be straight forward, it could be a volatile
         decsion related to the eventual import strategy(s) of the site.
         That is to say, if we pivot away from the CSV, it's possible that
         we might get QTI, which would contain a smattering of HTML nodes.
    
    With this commit, we're providing HTML.
    
    Related to:
    
    - https://github.com/scientist-softserv/viva/issues/174
    - https://github.com/scientist-softserv/viva/issues/175

--- app/models/question/essay.rb
+++ app/models/question/essay.rb
@@ -2,6 +2,9 @@
 
 ##
 # One question that involves a lengthy introduction to the concept and has a "Rich Text" ask.
+#
+# @note the {#data} attribute will be a Hash with one key: "html".  That "html" will be safe to
+#       render in the UI.
 class Question::Essay < Question
   self.type_name = "Essay"
 
--- app/models/question/upload.rb
+++ app/models/question/upload.rb
@@ -3,6 +3,9 @@
 ##
 # One question that involves a lengthy introduction to the concept and has a "Rich Text" ask; and
 # requires the answer to be an uploaded file.
+#
+# @note the {#data} attribute will be a Hash with one key: "html".  That "html" will be safe to
+#       render in the UI.
 class Question::Upload < Question
   self.type_name = "Upload"
 

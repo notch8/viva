7246089b322c66a5eff6e22ae5a276f4c12769af
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-12-08 14:00:00 -0500

    🎁 Add QTI XML export for Matching question
    On <2023-12-11 Mon> I verified with the client that the exported XML for
    Matching questions does import into Canvas.
    
    Related to:
    
    - https://github.com/scientist-softserv/viva/issues/166
    - https://github.com/scientist-softserv/viva/issues/170

---
+++ spec/views/question/matchings/_matching.xml.erb_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'question/matchings/_matching' do
+  let(:matching) { FactoryBot.build(:question_matching) }
+
+  it 'renders xml' do
+    render partial: "question/matchings/matching", locals: { matching: }
+    expect(rendered).to include(%(<item ident="#{matching.item_ident}" title="Question #{matching.id}" >))
+    # Unlike Question::Categorization; the matching does not allow for multiple.
+    # See https://github.com/scientist-softserv/viva/issues/237
+    expect(rendered).not_to include(%(rcardinality="Multiple">))
+    expect(rendered).to include(%(<fieldentry>matching_question</fieldentry>))
+  end
+end

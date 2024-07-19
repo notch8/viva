69f2f20697bdccde732822043b9a6ff7900d6237
Sarah Proctor <sarah.proctor@scientist.com>
2024-07-19 12:09:46 -0700

    🎁 Display images on each question
    This commit finishes the functionality of displaying images by adding width and margin styling properties.
    
    Ref:
    - https://github.com/scientist-softserv/viva/issues/277

--- app/javascript/components/ui/Question/index.jsx
+++ app/javascript/components/ui/Question/index.jsx
@@ -6,7 +6,7 @@ const Question = ({ text, title = 'Question', images }) => {
       <h2 className='h6 fw-bold'>{title}</h2>
       {images && images.map((image, index) => {
         return (
-          <img key={index} src={image.url} alt={image.alt_text} />
+          <img key={index} src={image.url} alt={image.alt_text} className='w-75 my-4'/>
         )
       })}
       <p>{text}</p>

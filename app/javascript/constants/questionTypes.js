ce2953a0dc6bf295e2dac097bbdfe9a1bb2ac6b6
Shana Moore <shana@scientist.com>
2024-12-09 11:45:40 -0800

    [i291] - add "create a question" section with dropdown of question types

---
+++ app/javascript/constants/questionTypes.js
@@ -0,0 +1,11 @@
+export const QUESTION_TYPE_NAMES = [
+  { key: 'Bow Tie', value: 'Bow Tie' },
+  { key: 'Categorization', value: 'Categorization' },
+  { key: 'Drag and Drop', value: 'Drag and Drop' },
+  { key: 'Essay', value: 'Essay' },
+  { key: 'Matching', value: 'Matching' },
+  { key: 'Traditional', value: 'Multiple Choice' },
+  { key: 'Select All That Apply', value: 'Select All That Apply' },
+  { key: 'Case Study', value: 'Stimulus Case Study' },
+  // { key: 'Upload', value: 'Upload' },
+];

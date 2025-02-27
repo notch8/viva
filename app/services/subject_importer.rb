a0f02bf7dcbd98d396e48917bb7a380950601cf9
LaRita Robinson <larita@scientist.com>
2025-02-27 13:48:32 -0500

    Adds subjects importer, rake task, and sample yaml

---
+++ app/services/subject_importer.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+
+class SubjectImporter
+  def self.import(file_path = 'config/subjects.yaml')
+    subjects_data = YAML.load_file(file_path)
+    subjects = subjects_data['subjects']['name']
+
+    subjects.each do |subject_name|
+      Subject.find_or_create_by(name: subject_name)
+    end
+  end
+end

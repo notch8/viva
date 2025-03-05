26b010390c0fb4e2daba55625fd607c45a358f77
Shana Moore <shana.lavina.moore@gmail.com>
2025-03-05 08:16:49 -0800

    styling changes

--- app/javascript/components/ui/Export/ExportModal.jsx
+++ app/javascript/components/ui/Export/ExportModal.jsx
@@ -17,8 +17,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=canvas"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-grid-3x3-gap-fill fs-4"></i>
-                <span>Canvas</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-grid-3x3-gap-fill fs-4 me-2"></i>
+                  <span>Canvas</span>
+                </div>
               </Button>
               
               <Button 
@@ -27,8 +29,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=blackboard"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-clipboard2-fill fs-4"></i>
-                <span>Blackboard</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-clipboard2-fill fs-4 me-2"></i>
+                  <span>Blackboard</span>
+                </div>
               </Button>
               
               <Button 
@@ -37,8 +41,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=brightspace"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-brightness-high-fill fs-4"></i>
-                <span>BrightSpace (D2L)</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-brightness-high-fill fs-4 me-2"></i>
+                  <span>BrightSpace (D2L)</span>
+                </div>
               </Button>
               
               <Button 
@@ -47,8 +53,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=moodle"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-mortarboard-fill fs-4"></i>
-                <span>Moodle</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-mortarboard-fill fs-4 me-2"></i>
+                  <span>Moodle</span>
+                </div>
               </Button>
             </div>
           </Tab>
@@ -61,8 +69,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=txt"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-file-text fs-4"></i>
-                <span>Plain Text</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-file-text fs-4 me-2"></i>
+                  <span>Plain Text</span>
+                </div>
               </Button>
               
               <Button 
@@ -71,8 +81,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=md"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-markdown fs-4"></i>
-                <span>Markdown</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-markdown fs-4 me-2"></i>
+                  <span>Markdown</span>
+                </div>
               </Button>
               
               <Button 
@@ -81,8 +93,10 @@ const ExportModal = ({ show, onHide, hasBookmarks }) => {
                 href="/bookmarks/export?format=xml"
                 disabled={!hasBookmarks}
               >
-                <i className="bi bi-filetype-xml fs-4"></i>
-                <span>QTI XML</span>
+                <div className="d-flex align-items-center">
+                  <i className="bi bi-filetype-xml fs-4 me-2"></i>
+                  <span>QTI XML</span>
+                </div>
               </Button>
             </div>
           </Tab>

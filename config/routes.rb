0f6dab47e21ef18d29e1924d12da0d40086619b5
Sarah Proctor <sproctor950@gmail.com>
2026-01-06 11:37:26 -0800

    🎁 Users cannot access the /register route
    New user creation is restricted to admin users. New users attempting to
    access the /register route will not be able to do so.
    
    Ref:
    - https://github.com/notch8/viva/issues/476

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }, :sign_out_via => [ :get, :delete ]
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout" }, :sign_out_via => [ :get, :delete ], skip: [:registrations]
 
   devise_scope :user do
     authenticated :user do

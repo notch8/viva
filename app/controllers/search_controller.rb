d2863b87df95ffba4d48e8c5d868214ce2531f68
Kirk Wang <k3wang@gmail.com>
2026-01-12 14:02:17 -0800

    :gift: Add pagination to search results
    Issue:
    - https://github.com/notch8/viva/issues/473

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,24 +3,38 @@
 ##
 # The controller to handle methods related to the search page.
 class SearchController < ApplicationController
+  include Pagy::Backend
+
   def index
     render inertia: 'Search', props: shared_props
   end
 
-  # private
+  private
 
   def shared_props
+    query = Question.filter_query(search: params[:search], **filter_values)
+
+    @pagy, @questions_page = pagy(query)
+
+    serialized_questions = Question.format_questions(@questions_page)
+
     {
       keywords: Keyword.names,
       subjects: Subject.names,
-      types: Question.type_names, # Deprecated Favor :type_names
+      types: Question.type_names,
       type_names: Question.type_names,
       levels: Level.names,
       selectedKeywords: params[:selected_keywords],
       selectedSubjects: params[:selected_subjects],
       selectedTypes: params[:selected_types],
       selectedLevels: params[:selected_levels],
-      filteredQuestions: Question.filter_as_json(search: params[:search], **filter_values),
+
+      # Pass the formatted page
+      filteredQuestions: serialized_questions,
+
+      # Pass the metadata
+      pagination: pagy_metadata(@pagy),
+
       bookmarkedQuestionIds: current_user.bookmarks.pluck(:question_id),
       lms: Question.lms
     }
